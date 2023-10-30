a24a50d8457f97527503a2ea43c7c2704a8c64c4
npetersen <npetersen@goldpointsystems.com>
2023-10-30 14:02:43 -0600

    Files with instructions

---
+++ crc.py
@@ -0,0 +1,13 @@
+### Part 2 of lab
+
+## Part 2a
+# *Write a program that generates an 8-bit CRC checksum for a data stream.
+# An example of this program follows, in pseudocode.
+# This same program may be used to check correct reception of the data stream.
+# The simplest data stream for which this may be used is two bytes;
+# simulate at least 5 different 2-byte data streams both with and without errors and log the results.
+# Particularly interesting are error data streams which differ by only 1 bit from the correct data.
+
+## Part 2b
+# Also, simulate at least one 32-byte data stream with at least one error and log the results.
+# Include a screenshot of the results and discuss the probability of an undetected error occurring
---
+++ parity.py
@@ -0,0 +1,12 @@
+### Part 1 of lab
+
+## Part 1a
+# Write a program (in any language, such as C, C++, Java, JavaScript, or Python)
+# that generates a parity bit (even or odd - you choose)
+# for each byte of data and verify its correct operation.*
+
+## Part 1b
+# Write another program to detect correct data based on the parity you have defined.
+# Then use the program to generate a byte of data along with the parity bit (9-bits total),
+# and observe what happens when there are 0, 1, 2, or 3 bits in error in a given byte.
+# Include a screenshot of the results (command line or other) and discuss the probability of an undetected error occurring.
